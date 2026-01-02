7e4e23ac1e8e135b9a21a703835287ac6d94e1f6
pias97 <ranabir.devnath@gmail.com>
2026-01-02 13:12:23 +0600

    Added installation shell scripts for terraform, docker,jenkins

---
+++ installation/Docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install docker.io -y
+
+sudo apt install docker-compose -y
+sudo chown $USER /var/run/docker.sock
+sudo usermod -aG docker $USER && newgrp docker
+echo "Docker installation complete. You may now run Docker commands."
